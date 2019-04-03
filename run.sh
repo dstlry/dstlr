28494a8ba1f0d080a17c58f5a9433eb5487c3be5
Ryan Clancy <ryan.clancy@uwaterloo.ca>
2019-04-03 00:22:49 -0400

    Remove old run script

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Start Neo4j
-docker run -d --publish=7474:7474 --publish=7687:7687 --volume=$NEO4J_DIR:/data -e NEO4J_dbms_memory_heap_max__size=32G -e NEO4J_dbms_memory_heap_initial__size=32G -e NEO4J_dbms_memory_pagecache_size=2G neo4j
