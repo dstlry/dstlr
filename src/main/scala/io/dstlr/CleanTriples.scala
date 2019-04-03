68022341d9315d88ac7a7b0ba5503e07966fae5a
Ryan Clancy <ryan.clancy@uwaterloo.ca>
2019-04-03 00:59:17 -0400

    Basic cleaning detection

--- src/main/scala/io/dstlr/CleanTriples.scala
+++ src/main/scala/io/dstlr/CleanTriples.scala
@@ -3,6 +3,7 @@ package io.dstlr
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.sql.SparkSession
 import org.neo4j.driver.internal.InternalNode
+import org.neo4j.spark.Neo4j
 
 /**
   * Enrich the "LINKS_TO" relationships of our extracted triples using data from WikiData.
@@ -20,16 +21,36 @@ object CleanTriples {
       .config("spark.neo4j.bolt.url", conf.neoUri())
       .config("spark.neo4j.bolt.user", conf.neoUsername())
       .config("spark.neo4j.bolt.password", conf.neoPassword())
+      .master("local[*]")
       .getOrCreate()
+
+    import spark.implicits._
 
     // Delete old output directory
     FileSystem.get(spark.sparkContext.hadoopConfiguration).delete(new Path(conf.output()), true)
 
-    org.neo4j.spark.Neo4j(spark.sparkContext)
-      .cypher("MATCH (e1:Entity)<-[:DATE_OF_BIRTH]-(e2:Entity)-[:LINKS_TO]->(u:URI)-[:DATE_OF_BIRTH]->(w:WikiDataValue) RETURN e1, e2, u, w")
+    val mapping = spark.read.option("header", "true").csv("wikidata.csv").as[WikiDataMappingRow]
+
+    Neo4j(spark.sparkContext)
+      .cypher("MATCH (d:Document)-->(s:Entity)-->(r:Relation {type: \"CITY_OF_HEADQUARTERS\"})-->(o:Entity) MATCH (s)-->(u:URI)-->(w:WikiDataValue {relation: r.type}) RETURN d, s, r, o, u, w")
       .loadNodeRdds
       .foreach(row => {
-        println(row.get(0).asInstanceOf[InternalNode].asMap())
+
+        val doc = row.get(0).asInstanceOf[InternalNode]
+        val sub = row.get(1).asInstanceOf[InternalNode]
+        val rel = row.get(2).asInstanceOf[InternalNode]
+        val obj = row.get(3).asInstanceOf[InternalNode]
+        val uri = row.get(4).asInstanceOf[InternalNode]
+        val wdv = row.get(5).asInstanceOf[InternalNode]
+
+        val observed = obj.get("label").asString()
+        val truth = wdv.get("value").asString()
+
+        if (observed == truth) {
+          println("Clean")
+        } else {
+          println("Dirty")
+        }
       })
 
     spark.stop()
