ff2b3b02c322c6ffa37294f505ccc232fde17b4a
Ryan Clancy <ryan.clancy@uwaterloo.ca>
2019-10-02 18:34:54 -0400

    Update load.sh

--- bin/load.sh
+++ bin/load.sh
@@ -2,7 +2,7 @@
 
 spark-submit --class io.dstlr.LoadTriples \
         --num-executors 1 --executor-cores 1 \
-        --driver-memory 16G --executor-memory 16G \
-        --conf spark.executor.heartbeatInterval=60 \
+        --driver-memory 8G --executor-memory 8G \
+        --conf spark.executor.heartbeatInterval=10000 \
         ../target/scala-2.11/dstlr-assembly-0.1.jar \
-        --input triples --neo4j.password password --neo4j.uri bolt://192.168.1.110:7687 --neo4j.batch.size 10000
+        --input triples --neo4j.password password --neo4j.uri bolt://localhost:7687 --neo4j.batch.size 10000
