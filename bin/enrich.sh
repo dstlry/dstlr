28c3344e4fdf9a2b4baacf7edd51578068f1454e
Ryan Clancy <ryan.clancy@uwaterloo.ca>
2019-10-02 18:34:05 -0400

    Update enrich.sh

--- bin/enrich.sh
+++ bin/enrich.sh
@@ -2,6 +2,6 @@
 
 spark-submit --class io.dstlr.EnrichTriples \
         --num-executors 1 --executor-cores 1 \
-        --driver-memory 64G --executor-memory 64G \
-        --conf spark.executor.heartbeatInterval=60 \
+        --driver-memory 8G --executor-memory 8G \
+        --conf spark.executor.heartbeatInterval=10000 \
         ../target/scala-2.11/dstlr-assembly-0.1.jar --input triples --output triples-enriched --partitions 1
