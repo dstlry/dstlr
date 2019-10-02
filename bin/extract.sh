ba2f17fbebfd15fe4535aec367d5374c7286fc5e
Ryan Clancy <ryan.clancy@uwaterloo.ca>
2019-10-02 18:31:56 -0400

    Update extract.sh

--- bin/extract.sh
+++ bin/extract.sh
@@ -5,5 +5,5 @@ spark-submit --class io.dstlr.ExtractTriples \
         --driver-memory 64G --executor-memory 48G \
         --conf spark.executor.heartbeatInterval=60 \
         --conf spark.executorEnv.JAVA_HOME=/usr/lib/jvm/java-9-openjdk-amd64 \
-        target/scala-2.11/dstlr-assembly-0.1.jar \
+        ../target/scala-2.11/dstlr-assembly-0.1.jar \
         --solr.uri 192.168.1.111:9983 --solr.index core18 --query *:* --partitions 2048 --output triples --sent-length-threshold 256
