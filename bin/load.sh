2d7b82a199c4aaf0723335c1b333493fc60094bb
Xinyu Mavis Liu <x389liu@gmail.com>
2019-10-09 16:31:57 -0400

    Update scripts (#25)
    After the changes, scripts could be running under dstlr/ instead of dstlr/bin/

--- bin/load.sh
+++ bin/load.sh
@@ -4,5 +4,5 @@ spark-submit --class io.dstlr.LoadTriples \
         --num-executors 1 --executor-cores 1 \
         --driver-memory 8G --executor-memory 8G \
         --conf spark.executor.heartbeatInterval=10000 \
-        ../target/scala-2.11/dstlr-assembly-0.1.jar \
+        target/scala-2.11/dstlr-assembly-0.1.jar \
         --input triples --neo4j.password password --neo4j.uri bolt://localhost:7687 --neo4j.batch.size 10000
